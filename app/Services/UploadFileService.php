cee7c23fe7a63c2ead075a676e40b8cbf05a6508
Guilherme Cabo <guilherme.pcabo@gmail.com>
2020-05-04 14:17:51 -0300

    Still trying to fix storage, changed how I upload files

--- app/Services/UploadFileService.php
+++ app/Services/UploadFileService.php
@@ -13,7 +13,6 @@ class UploadFileService
     public function execute(UploadedFile $file)
     {
         $filename = Str::uuid() . '.jpg';
-        $file->storeAs('public/tmp', $filename);
 
         $client = new Client([
             'base_uri' => 'http://api.medeirossouza.com',
@@ -23,13 +22,11 @@ class UploadFileService
             'multipart' => [
                 [
                     'name' => 'file',
-                    'contents' => fopen(public_path() . '/storage/tmp/' . $filename, 'r'),
+                    'contents' => fopen($file->getRealPath(), 'r'),
                     'filename' => $filename,
                 ]
             ]
         ]);
-
-        Storage::delete('/public/tmp/' . $filename);
 
         return $filename;
     }
